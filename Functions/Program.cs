d8d21f569e4575015d0e7545105685be57e8a06c
Vladislav Podlegajev <vladislav.podlegajev@tktk.ee>
2024-04-10 04:57:17 +0300

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoise = Console.ReadLine();
+
+if (userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+
+
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
